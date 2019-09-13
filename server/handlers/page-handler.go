c2098ace6db5f3bf4fbded3675d710fdf4744ced
Aniket <ani4545ket@gmail.com>
2019-09-13 21:59:15 +0530

    added sample code for html rendering

--- server/handlers/page-handler.go
+++ server/handlers/page-handler.go
@@ -1,8 +1,15 @@
 package handlers
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 //InitialRequest this is the first empty handler
 func InitialRequest(wr http.ResponseWriter, req *http.Request) {
-
+	tmpl, err := template.ParseFiles("client/sample.html")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.Execute(wr, nil)
 }
