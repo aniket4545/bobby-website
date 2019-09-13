affeeae65517cdfff11eed060f6c262b73b8ac25
Aniket <ani4545ket@gmail.com>
2019-09-13 20:54:36 +0530

    changing the setup

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	config "bobby-website/server/configurations"
-	h "bobby-website/server/handlers"
-	"fmt"
-	"net/http"
-)
-
 func main() {
-	fmt.Println("Server is listening on ", config.PORT)
-	http.HandleFunc("/", h.InitialRequest)
-	http.ListenAndServe(config.PORT, nil)
+
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,19 @@
 package server
+
+import (
+	config "bobby-website/server/configurations"
+	h "bobby-website/server/handlers"
+	"fmt"
+	"net/http"
+)
+
+//Start will start the server
+func Start() {
+	fmt.Println("Server is listening on ", config.PORT)
+	routers()
+	http.ListenAndServe(config.PORT, nil)
+}
+
+func routers() {
+	http.HandleFunc("/", h.InitialRequest)
+}
