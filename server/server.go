d8eeecb72c301f74465b5dbe4d3cb08cc5341286
Aniket <ani4545ket@gmail.com>
2019-09-14 19:46:36 +0530

    changing the folder structures and serving the files to client code added

--- server/server.go
+++ server/server.go
@@ -5,15 +5,20 @@ import (
 	h "bobby-website/server/handlers"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 //Start will start the server
 func Start() {
 	fmt.Println("Server is listening on ", config.PORT)
-	routers()
+	http.Handle("/", routers())
 	http.ListenAndServe(config.PORT, nil)
 }
 
-func routers() {
-	http.HandleFunc("/", h.InitialRequest)
+func routers() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", h.InitialRequest)
+	router.HandleFunc("/{file}", h.HandleFile)
+	return router
 }
