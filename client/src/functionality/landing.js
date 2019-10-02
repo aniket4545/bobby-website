26c575e2dda88988059fddd7ea99d2387fd91674
Takale shweta <shwetatakale112@gmail.com>
2019-10-02 17:57:30 +0530

    media queries

--- client/src/functionality/landing.js
+++ client/src/functionality/landing.js
@@ -1,15 +1,15 @@
 function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+  return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
 function showing() {
-    const panel = document.querySelectorAll('.splash-panel');
-    for (let i = 0; i < panel.length; i++) {
-      panel[i].classList.toggle('showing');
-    }
-    setTimeout(function () {
-      showing();
-    }, 3000);
+  const panel = document.querySelectorAll('.splash-panel');
+  for (let i = 0; i < panel.length; i++) {
+    panel[i].classList.toggle('showing');
+  }
+  setTimeout(function () {
+    showing();
+  }, 3000);
 }
 
 showing();
@@ -21,4 +21,15 @@ document.querySelector('.js-login-page').addEventListener('click', () => {
 
 document.querySelector('.js-cancel-login').addEventListener('click', () => {
   loginPageElement.style.display = 'none';
+});
+
+const mobileHeaderElement = document.querySelector('.js-mobile-actions');
+document.querySelector('.js-menu-icon').addEventListener('click', () => {
+  mobileHeaderElement.style.display = 'block';
+});
+
+window.addEventListener('mouseup', (event) => {
+  if (event.target != mobileHeaderElement && event.target.parentNode != mobileHeaderElement) {
+    mobileHeaderElement.style.display = 'none';
+  }
 });
