dbb384d457f48acb255b91138c077df85e30134f
Aniket <ani4545ket@gmail.com>
2020-03-22 18:15:26 +0530

    changes done for hrml

--- client/src/functionality/landing.js
+++ client/src/functionality/landing.js
@@ -5,11 +5,11 @@ function hasClass(element, cls) {
 function showing() {
   const panel = document.querySelectorAll('.splash-panel');
   for (let i = 0; i < panel.length; i++) {
-    panel[i].classList.toggle('showing');
+    panel[i].classList.toggle('showing'); 
   }
   setTimeout(function () {
     showing();
-  }, 3000);
+  }, 5000);
 }
 
 showing();
